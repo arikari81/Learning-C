1af55b564d23a1239918fe1f6fd0e3c6317684d0
arikari81 <abhipatnav@gmail.com>
2026-09-06 17:28:53 +0000

    code modified

--- classwork/september/ppsclass2sep26p2.c
+++ classwork/september/ppsclass2sep26p2.c
@@ -1,1 +1,19 @@
 /*chekcing for negative or positive number*/
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number to be checked: \n");
+    scanf("%d", &num);
+
+    if (num >= 0)
+    {
+        printf("The number is positive.\n");
+    }
+    else
+    {
+        printf("The number is negative.\n");
+    }
+
+    return 0;
+}
